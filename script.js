6b0ef4480dba2fd8b3b91f96075a71962b4200f8
Andrew <andrewmorris04@gmail.com>
2022-12-06 03:30:59 -0500

    working intersect

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let filmCount = 0;
-
-
-
-let cclist1;
-let cclist2;
+let filmCC = [];
 
 let firstdone = false;
 
@@ -47,28 +43,6 @@ async function getFilmTitle(name){
 }
 
 
-/*
-getFilmTitle(firstCall)
-.then(function(jsonData){
-  console.log(jsonData);
-  let firstID= JSON.stringify(jsonData.results[0]).substring(7,16);
-  console.log("ID: " + firstID);
-  cclist1 = cast(firstID); 
-   
-})
-.then(function(){
-  getFilmTitle(secondCall)
-  .then(function(jsonData){
-  console.log(jsonData);
-  let secondID= JSON.stringify(jsonData.results[0]).substring(7,16);
-  console.log("ID: " + secondID);
-  cclist2 = cast(secondID);
-  
-})
-
-})
-
-*/
 
 function cast(filmID){
   let cast1 = "https://imdb-api.com/en/API/FullCast/k_ljv5h5vz/" + filmID;
@@ -80,9 +54,9 @@ function cast(filmID){
     let response = JSON.stringify(jsonData);
     let matches = response.match(regex).map(x => x.replace('name":"',""));
     if(firstdone == false){
-      document.getElementById("data").innerHTML = matches;
+      filmCC.push(matches);
     } else{
-      document.getElementById("dataa").innerHTML = matches;
+      filmCC.push(matches);
       intersect();
     }
     firstdone = true;
@@ -99,16 +73,10 @@ function removeDups(arr) {
 
 async function intersect() {
 
-  const a = document.querySelector("#data").innerHTML;
-  const b = document.querySelector("#dataa").innerHTML;
-
-  const y = a.split(',');
-  const z = b.split(',');
-
-  const filteredArray = removeDups(y.filter(value => z.includes(value)));
+  const filteredArray = removeDups(a.filter(value => b.includes(value)));
   console.log("In Common: " + filteredArray);
-  document.getElementById("h3").innerHTML = "People in common: " + filteredArray;
-  document.getElementById("cloud").appendChild(document.createElement('img')).src = "https://quickchart.io/wordcloud?text=" + y + z;
+  //document.getElementById("h3").innerHTML = "People in common: " + filteredArray;
+  //document.getElementById("cloud").appendChild(document.createElement('img')).src = "https://quickchart.io/wordcloud?text=" + y + z;
 }
 
 
